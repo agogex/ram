6b838fa79ccd20c0f80e7bc8b00a2e1bbf831e2b
agoge <agoge@outlook.com>
2016-02-22 16:41:53 +0200

    Add an employee create form and filter

--- public/app/app.js
+++ public/app/app.js
@@ -2,10 +2,33 @@
 
 angular.module('app', ['appRouter'])
 	.controller('employeesController', function($scope, $http){
-		$http.get('/api/employees')
-			.then(function(data){
-				$scope.employees = data.data;
-			});
+		$scope.showCreateForm = false;
+		$scope.formData = {};
+		
+		$scope.getEmployees = function(){
+			$http.get('/api/employees')
+				.then(function(data){
+					$scope.employees = data.data;
+				});
+		};
+		
+		$scope.getEmployees();
+			
+		$scope.add = function(){
+			$scope.showCreateForm = !$scope.showCreateForm;
+		};
+		
+		$scope.createNewEpmloyee = function(){
+			$http.post('/api/employees', $scope.formData)
+				.success(function(){
+					$scope.getEmployees();
+					$scope.showCreateForm = false;
+				});
+		};
+		
+		angular.element(document).ready(function(){
+			componentHandler.upgradeAllRegistered();
+		});
     })
     .controller('homeController', function($scope){
         $scope.message = 'Home Page';
