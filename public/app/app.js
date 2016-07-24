2373bbfd030394e8b1462c76f0d759155464bb0a
agoge <agoge@outlook.com>
2016-07-24 18:33:33 +0000

    make some changes

--- public/app/app.js
+++ public/app/app.js
@@ -1,49 +1,50 @@
 /// <reference path="../../typings/angularjs/angular.d.ts" />
 
 angular.module('app', ['appRouter', 'ngAnimate'])
-    .controller('mainMenuController', function ($scope, $rootScope, $location, authentication) {
-        $scope.isActive = function (page) {
+    .controller('mainMenuController', function($scope, $rootScope, $location, authentication) {
+        $scope.isActive = function(page) {
             var currentPage = window.location.pathname.split('/')[1] || 'home';
             return page == currentPage;
         };
-        $scope.logout = function () {
+        $scope.logout = function() {
             authentication.logout();
             $rootScope.isLoggedIn = false;
             $location.path('/');
         };
         $rootScope.isLoggedIn = authentication.isLoggedIn();
     })
-    .controller('employeesController', function ($scope, $http, authentication) {
+    .controller('employeesController', function($scope, $http, authentication) {
         $scope.currentPage = true;
         $scope.showCreateForm = false;
         $scope.formData = {};
 
-        $scope.getEmployees = function () {
+        $scope.getEmployees = function() {
             $http.get('/api/employees')
-                .then(function (data) {
+                .then(function(data) {
                     $scope.employees = data.data;
+                    console.log($scope.employees);
                 });
         };
 
         $scope.getEmployees();
 
-        $scope.add = function () {
+        $scope.add = function() {
             $scope.showCreateForm = !$scope.showCreateForm;
         };
 
-        $scope.createNewEpmloyee = function () {
+        $scope.createNewEpmloyee = function() {
             // $http.defaults.headers.common.Authorization = 'Bearer '+ authentication.getToken();
             $http.post('/api/employees', $scope.formData, {
-                headers: {
-                    Authorization: 'Bearer ' + authentication.getToken()
-                }
-            })
-                .success(function () {
+                    headers: {
+                        Authorization: 'Bearer ' + authentication.getToken()
+                    }
+                })
+                .success(function() {
                     $scope.getEmployees();
                     $scope.showCreateForm = false;
                     Materialize.toast('Запис успішно створено!', 4000, 'teal lighten-2');
                 })
-                .error(function (data, status) {
+                .error(function(data, status) {
                     console.error('Status: ' + status + '. Data: ' + data);
                     var message = status == 401 ? 'Помилка авторизації' : 'При створенні запису виникла помилка!';
                     Materialize.toast(message, 4000, 'red lighten-2');
@@ -51,55 +52,51 @@ angular.module('app', ['appRouter', 'ngAnimate'])
             $scope.formData = {};
         };
 
-        $scope.showEditForm = function (showEdit) {
+        $scope.showEditForm = function(showEdit) {
             showEdit = !showEdit;
         };
 
-        $scope.editEmployee = function (employee) {
+        $scope.editEmployee = function(employee) {
             $http.put('/api/employees/' + employee._id, employee)
-                .success(function () {
+                .success(function() {
                     $scope.getEmployees();
                     Materialize.toast('Запис успішно збережено!', 4000, 'teal lighten-2');
                 })
-                .error(function (data, status) {
+                .error(function(data, status) {
                     console.error('Status: ' + status + '. Data: ' + data);
                     Materialize.toast('При збереженні запису виникла помилка!', 4000, 'red lighten-2');
                 });
         };
 
-        $scope.deleteEmployee = function (employeeId) {
+        $scope.deleteEmployee = function(employeeId) {
             $http.delete('/api/employees/' + employeeId)
-                .success(function () {
+                .success(function() {
                     $scope.getEmployees();
                     Materialize.toast('Запис успішно видалено!', 4000, 'teal lighten-2');
                 })
-                .error(function (data, status) {
+                .error(function(data, status) {
                     console.error('Status: ' + status + '. Data: ' + data);
                     Materialize.toast('При видаленні запису виникла помилка!', 4000, 'red lighten-2');
                 });
         }
-
-        // angular.element(document).ready(function(){
-        // 	componentHandler.upgradeAllRegistered();
-        // });
     })
-    .controller('homeController', function ($scope) {
+    .controller('homeController', function($scope) {
         $scope.message = 'Home Page';
     })
-    .controller('newsController', function ($scope, $http, $routeParams) {
+    .controller('newsController', function($scope, $http, $routeParams) {
         $scope.currentPage = true;
         $scope.showCreateForm = false;
         $scope.formData = {};
         $scope.pagesAmount = null;
 
-        $scope.isActive = function(pageNumber){
+        $scope.isActive = function(pageNumber) {
             return window.location.pathname.split('/').pop() == pageNumber;
         }
 
-        $scope.getNews = function () {
+        $scope.getNews = function() {
             var url = $routeParams.page ? '/api/news/page/' + $routeParams.page : '/api/news';
             $http.get(url)
-                .then(function (data) {
+                .then(function(data) {
                     $scope.pagesAmount = data.data.pop().pagesAmount;
                     $scope.news = data.data;
                 });
@@ -107,61 +104,61 @@ angular.module('app', ['appRouter', 'ngAnimate'])
 
         $scope.getNews();
 
-        $scope.add = function () {
+        $scope.add = function() {
             $scope.showCreateForm = !$scope.showCreateForm;
         };
 
-        $scope.createNews = function () {
+        $scope.createNews = function() {
             $http.post('/api/news', $scope.formData)
-                .success(function () {
+                .success(function() {
                     $scope.getNews();
                     $scope.showCreateForm = false;
                     Materialize.toast('Запис успішно створено!', 4000, 'teal lighten-2');
                 })
-                .error(function (data, status) {
+                .error(function(data, status) {
                     console.error('Status: ' + status + '. Data: ' + data);
                     Materialize.toast('При створенні запису виникла помилка!', 4000, 'red lighten-2');
                 });
             // $scope.formData = {};
         };
 
-        $scope.editNews = function (news) {
+        $scope.editNews = function(news) {
             $http.put('/api/news/' + news._id, news)
-                .success(function () {
+                .success(function() {
                     $scope.getNews();
                     Materialize.toast('Запис успішно збережено!', 4000, 'teal lighten-2');
                 })
-                .error(function (data, status) {
+                .error(function(data, status) {
                     console.error('Status: ' + status + '. Data: ' + data);
                     Materialize.toast('При збереженні запису виникла помилка!', 4000, 'red lighten-2');
                 });
         };
 
-        $scope.deleteNews = function (newsId) {
+        $scope.deleteNews = function(newsId) {
             $http.delete('/api/news/' + newsId)
-                .success(function () {
+                .success(function() {
                     $scope.getNews();
                     Materialize.toast('Запис успішно видалено!', 4000, 'teal lighten-2');
                 })
-                .error(function (data, status) {
+                .error(function(data, status) {
                     console.error('Status: ' + status + '. Data: ' + data);
                     Materialize.toast('При видаленні запису виникла помилка!', 4000, 'red lighten-2');
                 });
         }
     })
-    .controller('articleController', function ($scope, $http, $routeParams, $window) {
+    .controller('articleController', function($scope, $http, $routeParams, $window) {
         $http.get('/api/news/' + $routeParams.id)
-            .success(function (data) {
+            .success(function(data) {
                 $scope.news = data;
             })
-            .error(function (data) {
+            .error(function(data) {
                 console.error(data);
             });
-        $scope.back = function(){
+        $scope.back = function() {
             $window.history.back();
         };
     })
-    .controller('contactsController', function () {
+    .controller('contactsController', function() {
         var position = [50.4261108, 30.53702469999996];
 
         var latLng = new google.maps.LatLng(position[0], position[1]);
@@ -184,22 +181,21 @@ angular.module('app', ['appRouter', 'ngAnimate'])
             animation: google.maps.Animation.DROP
         });
     })
-    .controller('loginController', function($scope, $rootScope, $location, authentication){
+    .controller('loginController', function($scope, $rootScope, $location, authentication) {
         $scope.credentials = {
             username: "",
             password: ""
         };
-        $scope.login = function(){
+        $scope.login = function() {
             authentication.login($scope.credentials)
-                .success(function(){
+                .success(function() {
                     Materialize.toast('Авторизація пройшла успішно!', 4000, 'teal lighten-2');
                     $location.path('/');
                     $rootScope.isLoggedIn = true;
                 })
-                .error(function(err){
+                .error(function(err) {
                     Materialize.toast(err.message, 4000, 'red lighten-2');
                     console.log(err);
                 });
         };
     });
-
